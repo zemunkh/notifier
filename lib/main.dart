117bbd30cb73bd09b71083bcd832ac5c18749b77
Zorigbold  Munkh-Erdene <zemunkh@gmail.com>
2019-10-08 15:23:08 +0800

    two audio files can be read, now.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:notifier/utils.dart';
+import 'package:flutter/services.dart' show rootBundle;
+// import 'package:notifier/utils.dart';
+import 'package:audioplayer/audioplayer.dart';
+import 'package:path_provider/path_provider.dart';
+
 import 'package:notifier/numberLogic.dart';
 
 
-var audioTools = LocalAudioTools();
+typedef void OnError(Exception exception);
+
+// var audioTools = LocalAudioTools();
 
 
 void main() => runApp(new MyApp());
+
+enum PlayerState { stopped, playing, paused }
 
 class MyApp extends StatelessWidget {
   @override
@@ -29,52 +41,132 @@ class OrderNotify extends StatefulWidget {
 }
 
 class _OrderNotifyState extends State<OrderNotify> {
-  
-  List<String> entries = <String>['1', '2', '3', '4', '5', '6'];
+  List<String> entries = <String>[' ', ' ', ' ', ' ', ' ', ' '];
   final List<int> colorCodes = <int>[600, 500, 400, 300, 200, 100];
-
   final TextEditingController _myController = TextEditingController();
-
   String result = "";
+  
+
+  AudioPlayer audioPlayer;
+  PlayerState playerState = PlayerState.stopped;
+
+  get isPlaying => playerState == PlayerState.playing;
+  get isPaused => playerState == PlayerState.paused;
+  get isStopped => playerState == PlayerState.stopped;
+
+  List<String> audioFiles = <String>[]; 
+
+  StreamSubscription _audioPlayerStateSubscription;
+
+  final Map<String, String> _nameToPath = {};
+
+  @override
+  void initState() {
+    super.initState();
+    initAudioPlayer();
+  }
 
   @override
   void dispose() {
+    _audioPlayerStateSubscription.cancel();
     _myController.dispose();
     super.dispose();
   }
 
+  void initAudioPlayer() {
+    audioPlayer = new AudioPlayer();
+    _audioPlayerStateSubscription = audioPlayer.onPlayerStateChanged.listen((s){
+      if(s == AudioPlayerState.PLAYING) {
+        print("Still Playing...");
+      } else if(s == AudioPlayerState.STOPPED) {
+        onComplete();
+      }
+    }, onError: (msg) {
+      playerState = PlayerState.stopped;
+    });
+  }
+
+  Future loadFile(String name) async {
+    final bytes = await rootBundle.load('assets/audio/$name');
+    final dir = await getApplicationDocumentsDirectory();
+    final file = File('${dir.path}/$name');
+
+    await file.writeAsBytes(new Uint8List.view(bytes.buffer));
+    if (await file.exists()) setState((){_nameToPath[name] = file.path;}); 
+  }
+
+  Future loadTwoFile(String name0, String name1) async {
+    final bytes0 = await rootBundle.load('assets/audio/$name0');
+    final dir0 = await getApplicationDocumentsDirectory();
+    final file0 = File('${dir0.path}/$name0');
+
+    await file0.writeAsBytes(new Uint8List.view(bytes0.buffer));
+    if (await file0.exists()) setState((){_nameToPath[name0] = file0.path;});
+
+
+    final bytes1 = await rootBundle.load('assets/audio/$name1');
+    final dir1 = await getApplicationDocumentsDirectory();
+    final file1 = File('${dir1.path}/$name1');
+
+    await file1.writeAsBytes(new Uint8List.view(bytes1.buffer));
+    if (await file1.exists()) setState((){_nameToPath[name1] = file1.path;}); 
+  }
+
+  Future play(String name) async {
+    await audioPlayer.play(_nameToPath[name], isLocal: true);
+    setState(() => playerState = PlayerState.playing);
+  }
+
+  Future stop() async {
+    await audioPlayer.stop();
+    setState(() {
+      playerState = PlayerState.stopped;
+    });
+  }
+
+  void onComplete() {
+    setState((){
+      playerState = PlayerState.stopped;
+    });
+
+    print("##### #### ### Ready to Play next audio");
+    if(audioFiles.isNotEmpty) {
+      print("I am not Empty");
+      play(audioFiles[0]);
+      audioFiles.removeAt(0);
+    } else {
+      entries.insert(0, result);
+      entries.removeLast();
+    }
+
+  }
+
   void _playAudioNotifier(number) async {
+
     /// Check how many files will be called by digit
     /// 1. Get the number of digits from input values
     /// 2. Assign one digit and one time calling number to audio files
     /// 3. Assign two time calling number to audio files
     /// 4. Assign those numbers to audio player section, which decides how many tracks will be called.
-    List<String> audioFiles = <String>[]; 
     audioFiles = numberLogic(number);
     print('Files: #### $audioFiles');
 
+
     if(audioFiles.length == 1) {
-      audioTools.loadFile(audioFiles[0], null).then( (_) {
-        audioTools.playAudioLoop(audioFiles[0], null);
-        audioTools.loadFile(audioFiles[0], null);
+      if(!isPlaying) {
+        loadFile(audioFiles[0]).then((_){
+          play(audioFiles[0]);
+          audioFiles.removeAt(0);
+        });
+      }
         //play music callback
-        entries.insert(0, result);
-        entries.removeLast();
-      }); 
     } else if (audioFiles.length == 2) {
       print("##########  Two Files #######");
+      loadTwoFile(audioFiles[0], audioFiles[1]).then((_){
+        play(audioFiles[0]);
+        audioFiles.removeAt(0);
+      });
     }
-
-
-
-        // print('### Audio file = $secondAudio');
-        // audioTools.loadFile(firstAudio, secondAudio).then( (_) {
-        //   audioTools.playAudioLoop(firstAudio, secondAudio);
-
-        //   entries.insert(0, result);
-        //   entries.removeLast();
-        // });
-
   }
 
 
@@ -92,32 +184,62 @@ class _OrderNotifyState extends State<OrderNotify> {
                     children: <Widget>[
                       Expanded(
                         flex: 2,
-                        child: Center(
-                          child: Text('Order is ready'),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Color(0xffFFCC00),
+                            border: Border.all(width: 3.0, color: Colors.black87), 
+                          ),
+                          child:  Center(
+                            child: Text('Сая гарсан',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold,),
+                            ),
+                          ),
                         ),
                       ),
                       Expanded(
                         flex: 7,
-                        child: Center(child: Text(result,
-                          style: TextStyle(fontSize: 100.0,),),)
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border(
+                              right: BorderSide(width: 3.0, color: Colors.black87),
+                              top: BorderSide(width: 3.0, color: Colors.black87),
+                              left: BorderSide(width: 3.0, color: Colors.black87),
+                              bottom: BorderSide(width: 3.0, color: Colors.white),
+                            ),
+                          ),
+                          child: Center(child: Text(result,
+                            style: TextStyle(fontSize: 100.0,),),)
+                        ),
                       ),
                       Expanded(
                         flex: 1,  /// text entry and submit section
-                        child: new TextField(
-                          inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
-                          decoration: new InputDecoration(
-                            hintText: "Type here",
-                            fillColor: Colors.white,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border(
+                              right: BorderSide(width: 3.0, color: Colors.black87),
+                              top: BorderSide(width: 3.0, color: Colors.white),
+                              left: BorderSide(width: 3.0, color: Colors.black87),
+                              bottom: BorderSide(width: 3.0, color: Colors.black87),
+                            ),
                           ),
-                          controller: _myController,
-                          onSubmitted: (String str){
-                            setState((){
-                              result = str;
-                              // entries.add(result); play music callback
-                              _playAudioNotifier(result);
-                              _myController.clear();
-                            });
-                          }
+                          child: new TextField(
+                            inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+                            decoration: InputDecoration(
+                              hintText: '?',
+                              contentPadding: EdgeInsets.all(8.0),
+                            ),
+                            style: TextStyle(color: Colors.white),
+                            controller: _myController,
+                            onSubmitted: (String str){
+                              setState((){
+                                result = str;
+                                // entries.add(result); play music callback
+                                _playAudioNotifier(result);
+                                _myController.clear();
+                              });
+                            }
+                          ),
                         ),
                       ),
                     ],
@@ -130,23 +252,48 @@ class _OrderNotifyState extends State<OrderNotify> {
                     children: <Widget>[
                       Expanded(
                         flex: 2,
-                        child: Center(
-                          child: Text('Taken Order'),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Color(0xff5AC8FA),
+                            border: Border.all(width: 3.0, color: Colors.black87) 
+                          ),
+                          child:  Center(
+                            child: Text('Гарсан',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold,),
+                            ),
+                          ),
                         ),
                       ),
                       Expanded(
                         flex: 8,
-                        child: ListView.separated(
-                          padding: const EdgeInsets.all(8),
-                          itemCount: entries.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Container(
-                              height: 30,
-                              color: Colors.amber[colorCodes[index]],
-                              child: Center(child: Text('Entry ${entries[index]}')),
-                            );
-                          },
-                          separatorBuilder: (BuildContext context, int index) => const Divider(),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border(
+                              right: BorderSide(width: 3.0, color: Colors.black87),
+                              top: BorderSide(width: 3.0, color: Colors.black87),
+                              left: BorderSide(width: 3.0, color: Colors.black87),
+                              bottom: BorderSide(width: 3.0, color: Colors.black87),
+                            ),
+                          ),
+                          child: ListView.separated(
+                            padding: const EdgeInsets.all(8),
+                            itemCount: entries.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Container(
+                                height: 30,
+                                // color: Colors.amber[colorCodes[index]],
+                                child: Center(child: Text('${entries[index]}', 
+                                  style: TextStyle(fontSize: 30, 
+                                    color: Colors.blue[colorCodes[index]],
+                                    fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                            separatorBuilder: (BuildContext context, int index) => const Divider(),
+                          ),
                         ),
                       ),
                     ],
--- lib/utils.dart
+++ lib/utils.dart
@@ -8,47 +8,64 @@ import 'package:flutter/services.dart' show rootBundle;
 import 'package:audioplayer/audioplayer.dart';
 import 'package:path_provider/path_provider.dart';
 
+
+
 class LocalAudioTools {
   final AudioPlayer _audioPlayer = AudioPlayer();
-  final Map<String, String> _nameToPath = {};
 
-  Future loadFile(String name, String sname) async {
+  final Map<String, String> _nameToPath = {};
+  final Map<String, String> _nameToPath0 = {};
+  final Map<String, String> _nameToPath1 = {};
+
+
+  Future loadFile(String name) async {
     final bytes = await rootBundle.load('assets/audio/$name');
     final dir = await getApplicationDocumentsDirectory();
     final file = File('${dir.path}/$name');
 
     await file.writeAsBytes(new Uint8List.view(bytes.buffer));
     if (await file.exists()) _nameToPath[name] = file.path;
-
-    if(sname != null) {
-      final sbytes = await rootBundle.load('assets/audio/$sname');
-      final sdir = await getApplicationDocumentsDirectory();
-      final sfile = File('${sdir.path}/$sname');
-
-      await file.writeAsBytes(new Uint8List.view(sbytes.buffer));
-      if (await file.exists()) _nameToPath[sname] = sfile.path;
-    }
   }
 
-  void playAudioLoop(String name, String sname) {
+  Future loadTwoFile(String name0, String name1) async {
+    final bytes0 = await rootBundle.load('assets/audio/$name0');
+    final dir0 = await getApplicationDocumentsDirectory();
+    final file0 = File('${dir0.path}/$name0');
+
+    await file0.writeAsBytes(new Uint8List.view(bytes0.buffer));
+    if (await file0.exists()) _nameToPath0[name0] = file0.path;
+
+
+    final bytes1 = await rootBundle.load('assets/audio/$name1');
+    final dir1 = await getApplicationDocumentsDirectory();
+    final file1 = File('${dir1.path}/$name1');
+
+    await file1.writeAsBytes(new Uint8List.view(bytes1.buffer));
+    if (await file1.exists()) _nameToPath1[name1] = file1.path;
+  }
+
+  bool playAudioLoop(String name) {
     // restart audio if it has finished
     // _audioPlayer.setCompletionHandler(() => playAudio(name));
-    _audioPlayer.onPlayerStateChanged.listen((s){
-      if(s == AudioPlayerState.COMPLETED) {
-        print('Player Stopped');
-        if(sname != null) {
-          // playAudio(sname);
-          // playAudio(sname);
-        }
-      }
-    });
     playAudio(name);
+    // playerState = AudioPlayerState.COMPLETED;
     print('Audio is done!');
-    
+    return true;
   }
+
+
 
   Future<Null> playAudio(String name) async {
     await _audioPlayer.stop();
     await _audioPlayer.play(_nameToPath[name], isLocal: true);
+  }
+
+  Future<Null> playTwoAudio(String name0, String name1) async {
+    await _audioPlayer.stop();
+    await _audioPlayer.play(_nameToPath0[name0], isLocal: true);
+
+    // await _audioPlayer.stop();
+    // await _audioPlayer.play(_nameToPath1[name1], isLocal: true);
+    // playAudio(name1);
   }
 }
